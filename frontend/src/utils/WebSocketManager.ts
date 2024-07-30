c9a51b49f4180ed4cfc3e61929bbcca4d8a1af3a
Rohan Sharma <rohansharma@gmail.com>
2024-07-29 22:55:43 -0400

    Updated WebSocket Logic

--- frontend/src/utils/WebSocketManager.ts
+++ frontend/src/utils/WebSocketManager.ts
@@ -1,6 +1,7 @@
 export class WebSocketManager {
   private static instance: WebSocketManager;
   private ws: WebSocket;
+  private callbacks: any = {};
 
   private constructor() {
     this.ws = new WebSocket("ws://localhost:3001");
@@ -16,9 +17,38 @@ export class WebSocketManager {
 
   private init() {
     this.ws.onopen = () => {
-      console.log("connected");
+      console.log("connected"); 
       this.ws.send(JSON.stringify({ type: "list" }));
     };
+
+    this.ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      const type = data.type;
+      if(this.callbacks[type]){
+        if(type === "update"){
+          this.callbacks[type](data.scores);
+        }else if(type === "error"){
+          this.callbacks[type](data.message);
+        }else if(type === "rooms"){
+          this.callbacks[type](data.rooms);
+        }else if(type === "created"){
+          this.callbacks[type](data.roomId);
+        }
+      }
+      
+    }
+    }
+
+    sendMessage(message: any){
+      this.ws.send(JSON.stringify(message));
+    }
+
+    async registerCallback(type:string, callback:any){
+      this.callbacks[type] = callback;
+    }
+
+    async unregisterCallback(type:string){
+      delete this.callbacks[type];
     }
 
 
